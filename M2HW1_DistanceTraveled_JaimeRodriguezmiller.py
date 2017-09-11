dcc52697c57f8ecafe07e786801b4a5337c2dd5a
Jaime885 <31603343+Jaime885@users.noreply.github.com>
2017-09-11 18:51:48 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_JaimeRodriguezmiller.py
@@ -0,0 +1,22 @@
+#CTI-110
+#M2HW1-Distance Traveled
+#Jaime Rodriguezmiller
+#September 9, 2017
+
+#Value to the speed variable.
+speed = 70
+
+#Value to the time variable.
+time1 = 6
+time2 = 10
+time3 = 15
+
+#Get the distance traveled.
+distanceAfter6 = speed * time1
+distanceAfter10 = speed * time2
+distanceAfter15 = speed * time3
+
+print("Traveling at 70 miles per hour...")
+print("After 6 hours of driving you will have traveled ", distanceAfter6, "miles.")
+print("After 10 hours of driving you will have traveled ", distanceAfter10, "miles.")
+print("After 15 hours of driving you will have traveled ", distanceAfter15, "miles.")
