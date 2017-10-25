e577ae4e9aee37368a633e74f17a250777c818d1
Jaime885 <31603343+Jaime885@users.noreply.github.com>
2017-10-24 20:25:01 -0400

    Add files via upload

---
+++ m4lab1_Rodriguezmiller.py
@@ -0,0 +1,25 @@
+#CTI-110
+#M4LAB1-Shapes
+#Jaime Rodriguezmiller
+#October 22, 2017
+
+import turtle
+turtle.shape("turtle")
+
+t = 0
+while t < 1:
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    
+turtle.exitonclick
+
+
+    
+    
+    
+    
+    
